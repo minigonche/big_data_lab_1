f91e3b0151814679d6ff7d67a972aa6b10bb036a
makito <makitodev@outlook.com>
2018-09-02 19:38:20 -0500

    Actualización reto1 para usar json. Versión inicial y base de reto4

--- reto4/reto4_mapper.py
+++ reto4/reto4_mapper.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python
+# coding=utf-8
 """reto4_mapper.py"""
 #TODO
+import sys
+import re
+import json
+from datetime import datetime
+from datetime import timedelta
+
+#Mapper para el Reto 1 del Laboratorio 1 de Big Data
+
+# input comes from STDIN (standard input)
+#sys.stdin devuelve la entrada (palabras o archivo)
+for line in sys.stdin:
+    line = line.strip()
+    if('text' in line):
+        #Si la línea que está leyendo no está vacía
+        if line:
+            print '** línea con contenido **'
+            jsonObj = json.loads(line)
+            #Se obtiene el título del artículo
+            titulo = jsonObj['text']
+            #Número de palabras del título
+            num = len(titulo.split())
+            #Se imprime la tupla, titulo, num
+            print num
