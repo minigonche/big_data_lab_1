8d1f25abb58fde08c816e391dd62c15298253205
ap3rg <38160349+ap3rg@users.noreply.github.com>
2018-09-02 20:42:40 -0500

    Update reto3_mapper.py
    todavia dandole, no lo he corrido en hadoop

--- reto3/reto3_mapper.py
+++ reto3/reto3_mapper.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 """reto3_mapper.py"""
 #TODO
+'''
+Mapper para el reto 3 del laboratorio 1.
+
+Enunciado:
+Para cada pais en el cual se publican las noticias,
+indique cuantas noticias se publican y ciales so los autores de las noticias
+de ese pais
+'''
+
+import sys
+import json
+
+for line in sys.stdin:
+    line = line.strip()
+    if line:
+        data = json.loads(line)
+        country = data_j.get('thread').get('country')
+        author = data_j.get('author')
+
+        # if there are several authors, join them as a single csv string
+        if author.__class__ == list:
+            author = ",".join(author)
+
+        results = [country, author]
+        print("\t".join(results))
